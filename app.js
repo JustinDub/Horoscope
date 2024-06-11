991117934b6e7dbae0cdc39c0c6a590bf97b9934
JustinDub <justin.duban@hotmail.com>
2024-06-11 15:32:28 +0200

    menu page displays several date formats

--- app.js
+++ app.js
@@ -6,16 +6,25 @@ const port = 3000
 app.get('/', (req, res) => {
     let links = ``;
     const currentYear = new Date().getFullYear();
-    for(let year=currentYear; year>1990; year--)  links += `<li><a href="http://localhost:3000/horoscope/${year}">${year}</a></li>\n`;
-
+    for(let year=currentYear; year>1990; year--)  links += `<a href="http://localhost:3000/horoscope/${year}">${year}</a>\n`;
+    const isoFormat = "2002-09-30"
+    const stringFormat = "Thu Jan 01 1970 00:00:00 GMT+0000"
+    const longFormat = "December 17, 1995 03:24:00"
     const list = `
-        <h1>Find out your Zodiac sign !</h1>
+        <h1 style="margin-left: 25px">Find out your Zodiac sign !</h1>
         <h2>Click on a year and get the associated Zodiac sign, for example : 1998 is the ${getZodiacSign(1998)}'s year !</h2>
-        <ul>
+        <p style="margin-left: 25px">
             ${links}
+        </p>
+        <h2>You can also try more detailed date formats such as :</h2>
+        <ul>
+            <li><a href="http://localhost:3000/horoscope/${isoFormat}">${isoFormat}</a></li>
+            <li><a href="http://localhost:3000/horoscope/${stringFormat}">${stringFormat}</a></li>
+            <li><a href="http://localhost:3000/horoscope/${longFormat}">${longFormat}</a></li>
         </ul>
+        <h2>Want to test your own birthdate ?</h2>
+        <p style="margin-left: 25px">localhost:3000/horoscope/{your_birthdate}</p>
     `;
-
     res.status(200).send(list)
 })
 
